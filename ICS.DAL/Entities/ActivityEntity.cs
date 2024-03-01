f054697a34bd5cb4d5c6f7790fa4081c69d092c6
Albert Popov <govman32@gmail.com>
2024-03-02 00:21:06 +0100

    feat: add solution

---
+++ ICS.DAL/Entities/ActivityEntity.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ICS.DAL
+{
+    public class ActivityEntity : IEntity
+    {
+        public int activityId { get; set; }
+        public string name { get; set; } = string.Empty;
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public string room { get; set; } = string.Empty;
+        public string activityTypeTag { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public int subjectId { get; set; }
+        public SubjectEntity? subject { get; set; }
+        public RatingEntity? rating { get; set; }
+
+        public required Guid Id { get; set; }
+
+    }
+}
---
+++ ICS.DAL/Migrations/20240301230052_InitialCreate.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    studentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    firstName = table.Column<string>(type: "TEXT", nullable: false),
+                    lastName = table.Column<string>(type: "TEXT", nullable: false),
+                    fotoURL = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.studentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    subjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    abbreviation = table.Column<string>(type: "TEXT", nullable: false),
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.subjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    activityId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    start = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    end = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    room = table.Column<string>(type: "TEXT", nullable: false),
+                    activityTypeTag = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false),
+                    subjectId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.activityId);
+                    table.ForeignKey(
+                        name: "FK_Activities_Subjects_subjectId",
+                        column: x => x.subjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "subjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentSubject",
+                columns: table => new
+                {
+                    studentsstudentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    subjectssubjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubject", x => new { x.studentsstudentId, x.subjectssubjectId });
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Students_studentsstudentId",
+                        column: x => x.studentsstudentId,
+                        principalTable: "Students",
+                        principalColumn: "studentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Subjects_subjectssubjectId",
+                        column: x => x.subjectssubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "subjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rating",
+                columns: table => new
+                {
+                    ratingId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    points = table.Column<int>(type: "INTEGER", nullable: false),
+                    note = table.Column<string>(type: "TEXT", nullable: false),
+                    activityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    studentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rating", x => x.ratingId);
+                    table.ForeignKey(
+                        name: "FK_Rating_Activities_activityId",
+                        column: x => x.activityId,
+                        principalTable: "Activities",
+                        principalColumn: "activityId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rating_Students_studentId",
+                        column: x => x.studentId,
+                        principalTable: "Students",
+                        principalColumn: "studentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_subjectId",
+                table: "Activities",
+                column: "subjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_activityId",
+                table: "Rating",
+                column: "activityId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_studentId",
+                table: "Rating",
+                column: "studentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubject_subjectssubjectId",
+                table: "StudentSubject",
+                column: "subjectssubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rating");
+
+            migrationBuilder.DropTable(
+                name: "StudentSubject");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
