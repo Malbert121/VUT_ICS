b42733257338825af20c5b9c5c45b27c673f8981
Albert Popov <govman32@gmail.com>
2024-03-02 19:07:21 +0100

    review 1

---
+++ ICS.DAL/Entities/StudentEntity.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ICS.DAL
+{
+    public class StudentEntity : IEntity
+    {
+        public required Guid Id { get; set; }
+        public int studentId { get; set; }
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; } = string.Empty;
+        public string fotoURL { get; set; } = string.Empty;
+        public ICollection<SubjectEntity> subjects { get; set; } = new List<SubjectEntity>();
+
+    }
+}
