b77dcaaa0ba170ec67e51f718e73523a6b8f1a89
Nadzeya <xantsi00@vutbr.cz>
2024-03-14 14:49:46 +0100

    ActivityRatingMappers

---
+++ ICS.BL/Mappers/RatingModelMapper.cs
@@ -0,0 +1,47 @@
+﻿using ICS.DAL;
+
+namespace ICS.BL.Mappers
+{
+    public class RatingModelMapper : ModelMapperBase<RatingEntity, RatingListModel, RatingDetailModel>, IRatingModelMapper
+    {
+        public override RatingListModel MapToListModel(RatingEntity? entity)
+             => entity is null
+            ? RatingListModel.Empty
+            : new RatingListModel
+            {
+                Id = entity.Id,
+                Points = entity.points,
+ 
+            };
+
+        public override RatingDetailModel MapToDetailModel(RatingEntity? entity)
+            => entity is null
+            ? RatingDetailModel.Empty
+            : new RatingDetailModel
+            {
+                Id = entity.Id,
+                Points = entity.points,
+                Note = entity.note,
+                ActivityId = entity.activityId,
+                Activity = entity.activity,
+                StudentId = entity.studentId,
+                Student = entity.student
+                
+            };
+
+        public override RatingEntity MapToEntity(RatingDetailModel model)
+        {
+            return new RatingEntity
+            {
+                Id = model.Id,
+                points = model.Points,
+                note = model.Note,
+                activityId = model.ActivityId,
+                activity = model.Activity,
+                studentId = model.StudentId,
+                student = model.Student
+            };
+        }
+
+    }
+}
