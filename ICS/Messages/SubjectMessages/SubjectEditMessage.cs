759fd72aba1ebd50fb98f35aa7779fff0635ae9b
usennon <mikhailvorobevandr@gmail.com>
2024-05-01 20:02:00 +0300

    added initial subject view

---
+++ ICS/Messages/SubjectMessages/SubjectEditMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICS.Messages.SubjectMessages
+{
+    public record class SubjectEditMessage
+    {
+        public required Guid SubjectId { get; init; }
+    }
+}
---
+++ ICS/ViewModel/Subject/SubjectDetailEditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using ICS.Messages.SubjectMessages;
+using ICS.Services;
+using ICS.BL.Models;
+using ICS.BL.Facade;
+using ICS.BL.Facade.Interface;
+
+namespace ICS.ViewModel.Subject;
+
+[QueryProperty(nameof(Subject), nameof(Subject))]
+public partial class SubjectEditViewModel(
+    ISubjectFacade subjectFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService)
+{
+    public SubjectDetailModel Subject { get; init; } = SubjectDetailModel.Empty;
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await subjectFacade.SaveAsync(Subject);
+
+        MessengerService.Send(new SubjectEditMessage { SubjectId = Subject.Id });
+
+        navigationService.SendBackButtonPressed();
+    }
+}
