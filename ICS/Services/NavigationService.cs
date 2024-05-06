6ad7bc607b7d1f4608398d3eea0796edb92db48f
lilitmovsesian <lilitmovsesian@proton.me>
2024-05-06 23:02:36 +0200

    fix: fixed design

--- ICS/Services/NavigationService.cs
+++ ICS/Services/NavigationService.cs
@@ -17,30 +17,24 @@ public class NavigationService : INavigationService
     {
         new("//ratings", typeof(RatingListView), typeof(RatingListViewModel)),
         new("//ratings/detail", typeof(RatingDetailView), typeof(RatingDetailViewModel)),
-
         new("//ratings/edit", typeof(RatingEditView), typeof(RatingEditViewModel)),
         new("//ratings/detail/edit", typeof(RatingEditView), typeof(RatingEditViewModel)),
 
         new("//activities", typeof(ActivityListView), typeof(ActivityListViewModel)),
         new("//activities/detail", typeof(ActivityDetailView), typeof(ActivityDetailViewModel)),
-
         new("//activities/edit", typeof(ActivityEditView), typeof(ActivityEditViewModel)),
         new("//activities/detail/edit", typeof(ActivityEditView), typeof(ActivityEditViewModel)),
 
         new("//subjects", typeof(SubjectListView), typeof(SubjectListViewModel)),
         new("//subjects/detail", typeof(SubjectDetailView), typeof(SubjectDetailViewModel)),
-        new("//subjects/edit", typeof(SubjectAddNew), typeof(SubjectEditViewModel)),
+        new("//subjects/edit", typeof(SubjectEditView), typeof(SubjectEditViewModel)),
+        new("//subjects/detail/edit", typeof(SubjectEditView), typeof(SubjectEditViewModel)),
 
         new("//students", typeof(StudentListView), typeof(StudentListViewModel)),
         new("//students/detail", typeof(StudentDetailView), typeof(StudentDetailViewModel)),
         new("//students/edit", typeof(StudentEditView), typeof(StudentEditViewModel)),
-        //new("//subjects/detail/edit", typeof(SubjectEditView), typeof(RatingEditViewModel)),
-        //new("//recipes/detail", typeof(RecipeDetailView), typeof(RecipeDetailViewModel)),
+        new("//students/detail/edit", typeof(StudentEditView), typeof(StudentEditViewModel)),
 
-        //new("//recipes/edit", typeof(RecipeEditView), typeof(RecipeEditViewModel)),
-        //new("//recipes/detail/edit", typeof(RecipeEditView), typeof(RecipeEditViewModel)),
-
-        //new("//recipes/detail/edit/ingredients", typeof(RecipeIngredientsEditView), typeof(RecipeIngredientsEditViewModel)),
     };
 
     public async Task GoToAsync<TViewModel>()
---
+++ ICS/View/Subject/SubjectEditView.xaml.cs
@@ -0,0 +1,11 @@
+using ICS.ViewModel.Subject;
+
+namespace ICS.View.Subject;
+
+public partial class SubjectEditView
+{
+    public SubjectEditView(SubjectEditViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
