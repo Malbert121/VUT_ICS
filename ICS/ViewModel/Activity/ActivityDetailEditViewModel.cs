1f98b7ce2674cac4c9408fd8fd9c8fa99159f9ce
usennon <mikhailvorobevandr@gmail.com>
2024-05-15 19:30:14 +0300

    feat: possibility of time choosing

--- ICS/ViewModel/Activity/ActivityDetailEditViewModel.cs
+++ ICS/ViewModel/Activity/ActivityDetailEditViewModel.cs
@@ -18,6 +18,59 @@ public partial class ActivityEditViewModel(
     public ActivityDetailModel Activity { get; init; } = ActivityDetailModel.Empty;
     public SubjectDetailModel Subject { get; init; }
 
+    private DateTime _startDate;
+    private TimeSpan _startTime;
+    private DateTime _endDate;
+    private TimeSpan _endTime;
+    public DateTime StartDate
+    {
+        get => _startDate;
+        set
+        {
+            if (SetProperty(ref _startDate, value))
+            {
+                Activity.start = new DateTime(StartDate.Year, StartDate.Month, StartDate.Day, StartTime.Hours, StartTime.Minutes, 0);
+            }
+        }
+    }
+
+    public TimeSpan StartTime
+    {
+        get => _startTime;
+        set
+        {
+            if (SetProperty(ref _startTime, value))
+            {
+                Activity.start = new DateTime(StartDate.Year, StartDate.Month, StartDate.Day, StartTime.Hours, StartTime.Minutes, 0);
+            }
+        }
+    }
+
+    public DateTime EndDate
+    {
+        get => _endDate;
+        set
+        {
+            if (SetProperty(ref _endDate, value))
+            {
+                Activity.end = new DateTime(EndDate.Year, EndDate.Month, EndDate.Day, EndTime.Hours, EndTime.Minutes, 0);
+            }
+        }
+    }
+
+    public TimeSpan EndTime
+    {
+        get => _endTime;
+        set
+        {
+            if (SetProperty(ref _endTime, value))
+            {
+                Activity.end = new DateTime(EndDate.Year, EndDate.Month, EndDate.Day, EndTime.Hours, EndTime.Minutes, 0);
+            }
+        }
+    }
+
+
     [RelayCommand]
     private async Task SaveAsync()
     {
